2454d14ed0af63817f180941339108fed5fa13c0
Nathan Holt <nateofakin@gmail.com>
2024-01-24 16:04:01 -0500

    Simplified yml and app.js

--- application/app.js
+++ application/app.js
@@ -1,15 +1,12 @@
 const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
 
 const client = new DynamoDBClient({
-  region: 'us-west-2',
+  region: 'us-east-1',
   endpoint: 'http://localhost:8000/',
-  credentials: {
-    accessKeyId: 'xxx',
-    secretAccessKey: 'yyy',
-  },});
+});
 
  const main = async () => {
-  console.log("Code to run goes here")
+  console.log("I am alive!!!")
 };
 
 main()
